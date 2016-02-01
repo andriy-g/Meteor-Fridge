0155cea206a79f1d992af19a8d87fb5b7f08ec26
andriy-g <drey.galygin@gmail.com>
2016-01-31 20:28:45 -0800

    CSS + HTML Files done. Js File still work in progress.

--- MyFridge/MyFridge.js
+++ MyFridge/MyFridge.js
@@ -1,23 +1,107 @@
+Products = new Mongo.Collection('products');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.fridge.helpers({
+    products: function () {
+      return Products.find({
+        place: 'fridge'
+      });
+    }
+  });
+  Template.productList.helpers({
+    products: function () {
+      return Products.find({
+        place: 'supermarket'
+      });
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+
+  Template.fridge.onRendered(function () {
+    var templateInstance = this;
+
+    templateInstance.$('#fridge').droppable({
+      drop: function (evt, ui) {
+        var query = {
+          _id: ui.draggable.data('id')
+        };
+        var changes = {
+          $set: {
+            place: 'fridge'
+          }
+        };
+        Products.update(query, changes);
+      }
+    });
   });
+
+  Template.productList.onRendered(function () {
+    var templateInstance = this;
+
+    templateInstance.$('#supermarket').droppable({
+      drop: function (evt, ui) {
+        var query = {
+          _id: ui.draggable.data('id')
+        };
+        var changes = {
+          $set: {
+            place: 'supermarket'
+          }
+        };
+        Products.update(query, changes);
+      }
+    });
+  });
+  Template.fridge.onRendered(function () {
+    var templateInstance = this;
+    templateInstance.$('#fridge').droppable({
+      drop: function(evt, ui) {
+        var query = {_id: ui.draggable.data('id')};
+        var changes = { $set: {place: 'fridge'} };
+      }
+    });
+  });
+
+
+  Template.productListItem.onRendered(function () {
+    var templateInstance = this;
+
+    templateInstance.$('.draggable').draggable({
+      cursor: 'move',
+      helper: 'clone'
+    });
+  });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Products.remove({});
+
+    // fill the database with some products
+    Products.insert({
+      name: 'Milk',
+      img: '/milk.png',
+      place: 'fridge'
+    });
+
+    Products.insert({
+      name: 'Juice',
+      img: '/juice.png',
+      place: 'supermarket'
+    });
+
+    Products.insert({
+      name: 'Bread',
+      img: '/bread.png',
+      place: 'supermarket'
+    });
+
+    Products.insert({
+      name: 'Banana',
+      img: '/banana.png',
+      place: 'supermarket'
+    });
   });
 }
